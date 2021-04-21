47fac87674858cef829a2d640bcdeb92eb4e4385
AustinGoff <72319938+AustinGoff@users.noreply.github.com>
2021-04-20 22:15:15 -0500

    Add files via upload
    if you want to work on the fade engine alone to refine the engine without having to type in the shell for it here is a standalone file to work on

---
+++ fade.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+import java.util.Timer;
+
+public class fade {
+    public static void engine(){
+        int a=0, b=0, c=0, x=255, y=0, z=0,count=0;
+        Color start = new Color(a, b, c);
+        Color end = new Color(x, y, z);
+        Timer run = new Timer();
+
+        do {
+            if (a > x) {
+                a--;
+            } else if (a < x) {
+                a++;
+            }
+            if (b > y) {
+                b--;
+            } else if (b < y) {
+                b++;
+            }
+            if (c > z) {
+                c--;
+            } else if (c < z) {
+                c++;
+            }
+            start = new Color(a,b,c);
+            count++;
+            System.out.println(count);
+        } while (!start.equals(end));
+        System.out.println("number of times looped "+count);
+    }
+    public static void main(String[] args){
+        engine();
+    }
+}
