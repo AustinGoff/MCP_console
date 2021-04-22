05e885ac670679298c2f4689d623c06bb53c9f7b
AustinGoff <gbVyTBuGSUwLg8X>
2021-04-21 21:35:40 -0500

    setting up timer: proof that timer loops and waits on times

--- fade.java
+++ fade.java
@@ -5,10 +5,11 @@ public class fade {
     public static void engine(){
         int a=0, b=0, c=0, x=255, y=0, z=0,count=0;
         float run=3, moves;
+
         Color start = new Color(a, b, c);
         Color end = new Color(x, y, z);
-        Timer task = new Timer();
-
+        long startTime = System.nanoTime();
+    //loop to calculate moves needed to fade to color
         do {
             if (a > x) {
                 a--;
@@ -29,9 +30,30 @@ public class fade {
             count++;
             //System.out.println(count);
         } while (!start.equals(end));
-        //System.out.println("number of times looped "+count);
+
+        long endTime = System.nanoTime();
+        long duration = (endTime-startTime);
+        //duration = duration/1000000;
+        System.out.println("number of times looped "+count+" in "+duration+" nanoseconds");
         moves= count/run;
         System.out.println("moves needed per second "+moves);
+
+        a=0;
+        while (a<count){
+            startTime = System.nanoTime();
+            for (int i=0;i<=moves;i++) {
+                //System.out.println("timed loop: "+i);
+            }
+            try {
+                Thread.sleep(1000);
+            }catch(InterruptedException ie){
+                Thread.currentThread().interrupt();
+            }
+            endTime = System.nanoTime();
+            a+=moves;
+            System.out.println("timed loop2: "+a+" in "+ (endTime-startTime)/1000000000+ " sec");
+        }
+        System.exit(0);
     }
     public static void main(String[] args){
         engine();
